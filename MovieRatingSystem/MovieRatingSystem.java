33b8d813021649df045fea4cd7142c802f4bc744
AKC <akchamblin@gmail.com>
2025-03-10 02:04:48 +0000

    Step 1: Completed

---
+++ MovieRatingSystem/MovieRatingSystem.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MovieRatingSystem {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String name;
+
+        System.out.println("Welcome to the Movie Rating System");
+        System.out.println("\nWhat is your name?");
+        name = input.nextLine();
+        
+    }
+}
